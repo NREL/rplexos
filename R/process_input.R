e990e989e5a1936c625dadff43de2df19e5833c8
eibanez <eduardo.ibanez@nrel.gov>
2015-01-07 23:04:04 -0700

    Add object, class, property views (#4)

--- R/process_input.R
+++ R/process_input.R
@@ -48,6 +48,9 @@ process_input <- function(file) {
   new_database(dbf, xml.content, is.solution = FALSE)
   rm(xml.content)
   
+  # Add a few tables that will be useful later on
+  add_extra_tables_input(dbf)
+  
   # Close database connections
   dbDisconnect(dbf$con)
   
@@ -56,4 +59,61 @@ process_input <- function(file) {
   
   # Return the name of the database that was created
   invisible(db.name)
+}
+
+# Add a few tables that will be useful later on
+add_extra_tables_input <- function(db) {
+  rplexos_message("Adding extra tables to the database")
+  
+  # View with class and class_group
+  sql <- "CREATE VIEW [class] AS
+          SELECT c.class_id class_id,
+                 c.name,
+                 g.name class_group,
+                 c.is_enabled,
+                 c.description
+          FROM t_class c
+          INNER JOIN t_class_group g
+            ON c.class_group_id = g.class_group_id"
+  dbGetQuery(db$con, sql)
+  
+  # View with object, category, class, class_group
+  sql <- "CREATE VIEW [object] AS
+          SELECT o.object_id,
+                 o.name name,
+                 o.description,
+                 cat.name category,
+                 c.class_group,
+                 c.name class
+          FROM t_object o
+          JOIN [class] c
+            ON o.class_id = c.class_id
+          JOIN t_category cat
+            ON o.category_id = cat.category_id"
+  dbGetQuery(db$con, sql)
+  
+  # View with property
+  sql <- "CREATE VIEW [property] AS
+          SELECT p.property_id,
+                 p.name,
+                 p.default_value,
+                 p.period_type_id,
+                 p.is_enabled,
+                 p.is_dynamic,
+                 p.is_multi_band,
+                 p.max_band_id,
+                 pg.name property_group,
+                 c.name collection,
+                 u.value unit,
+                 p.description
+          FROM t_property p
+          JOIN t_property_group pg
+            ON p.property_group_id = pg.property_group_id
+          JOIN t_collection c
+            ON c.collection_id = p.collection_id
+         JOIN t_unit u
+           ON u.unit_id = p.unit_id"
+  dbGetQuery(db$con, sql)
+  
+  0
 }
