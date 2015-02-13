9ff90345e8a68dc23ff3840d5f98360fef959640
Eduardo Ibanez <edu.ibanez@gmail.com>
2015-02-13 10:06:59 -0700

    Update parallel functions

--- R/parallel.R
+++ R/parallel.R
@@ -1,3 +1,4 @@
+# Variable to hold cluster info and function to do parallelization
 .rplexos.cluster <- NULL
 `%dp%` <- foreach::`%do%`
 
@@ -19,33 +20,40 @@
 #' \dontrun{start_parallel_rplexos(3)}
 #' @export
 start_parallel_rplexos <- function(ncores = 1) {
-    # Check inputs
-    assert_that(is.count(ncores))
-    
-    # If one cluster is selected, turn of parallel capabilities
-    if (ncores == 1) {
-        if (!is.null(.rplexos.cluster)) {
-            parallel::stopCluster(.rplexos.cluster)
-            .rplexos.cluster <<- NULL
-            `%dp%` <- foreach::`%do%`
-        }
-        
-        return(invisible(1))
+  # Check inputs
+  assert_that(is.count(ncores), ncores >= 1)
+  
+  # If one cluster is selected, turn of parallel capabilities
+  if (ncores == 1) {
+    if (!is.null(.rplexos.cluster)) {
+      parallel::stopCluster(.rplexos.cluster)
+      .rplexos.cluster <<- NULL
+      `%dp%` <<- foreach::`%do%`
     }
     
-    # Make sure you don't start more cores that available
-    max.cores <- parallel::detectCores()
-    if(ncores > max.cores)
-      ncores <- max.cores
-    
-    # Create cluster with desired number of cores
-    .rplexos.cluster <<- parallel::makeCluster(ncores)
-    
-    # Register cluster
-    doParallel::registerDoParallel(.rplexos.cluster)
-    `%dp%` <- foreach::`%dopar%`
+    return(invisible(1))
+  }
+  
+  # Make sure you don't start more cores that available
+  max.cores <- parallel::detectCores()
+  if(ncores > max.cores)
+    ncores <- max.cores
+  
+  # Create cluster with desired number of cores
+  .rplexos.cluster <<- parallel::makeCluster(ncores)
+  
+  # Register cluster
+  doParallel::registerDoParallel(.rplexos.cluster)
+  `%dp%` <<- foreach::`%dopar%`
+  
+  invisible(ncores)
+}
 
-    invisible(ncores)
+
+#' @rdname start_parallel_rplexos
+#' @export
+stop_parallel_rplexos <- function() {
+  start_parallel_rplexos(1)
 }
 
 #' @rdname start_parallel_rplexos
