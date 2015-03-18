7a2d18c078ace2031dd063e44129b48edb53cac7
Eduardo Ibanez <edu.ibanez@gmail.com>
2015-03-18 00:20:00 -0600

    Quick query of classes and objects (#20)

--- R/query_summary.R
+++ R/query_summary.R
@@ -4,8 +4,8 @@ get_samples <- function(db) {
   sql <- "SELECT DISTINCT phase_id, period_type_id AS is_summary, sample FROM key"
   query_sql(db, sql) %>%
     add_phase_names %>%
-    select(phase_id, phase, is_summary, sample) %>%
-    arrange(phase_id, is_summary)
+    select(scenario, position, phase_id, phase, is_summary, sample) %>%
+    arrange(position, phase_id, is_summary)
 }
 
 
@@ -13,17 +13,36 @@ get_timeslices <- function(db) {
   sql <- "SELECT DISTINCT phase_id, period_type_id AS is_summary, timeslice FROM key"
   query_sql(db, sql) %>%
     add_phase_names %>%
-    select(phase_id, phase, is_summary, timeslice) %>%
-    arrange(phase_id, is_summary)
+    select(scenario, position, phase_id, phase, is_summary, timeslice) %>%
+    arrange(position, phase_id, is_summary)
 }
 
 get_bands <- function(db) {
   sql <- "SELECT DISTINCT phase_id, period_type_id AS is_summary, band FROM key"
   query_sql(db, sql) %>%
     add_phase_names %>%
-    select(phase_id, phase, is_summary, band) %>%
-    arrange(phase_id, is_summary)
+    select(scenario, position, phase_id, phase, is_summary, band) %>%
+    arrange(position, phase_id, is_summary)
 }
+
+get_classes <- function(db) {
+  sql <- "SELECT DISTINCT class_group, class FROM key"
+  query_sql(db, sql) %>%
+    select(-filename) %>%
+    arrange(position, class_group, class)
+}
+
+get_class_members <- function(db, class) {
+  sql <- sprintf("SELECT DISTINCT name, parent, region, zone FROM key WHERE class = '%s'", class)
+  query_sql(db, sql) %>%
+    select(-filename) %>%
+    arrange(position, name)
+}
+
+get_generators <- function(db) get_class_members(db, "Generator")
+get_regions    <- function(db) get_class_members(db, "Region")
+get_zones      <- function(db) get_class_members(db, "Zone")
+
 
 # Shortcut to add phase names to a result
 add_phase_names <- function(x) {
