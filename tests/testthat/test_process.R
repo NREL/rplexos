ab53b9fe095085cf06d5b99135b28867a7a73037
Eduardo Ibanez <eduardo.ibanez@nrel.gov>
2015-04-07 00:11:50 -0600

    Test processing files

---
+++ tests/testthat/test_process.R
@@ -0,0 +1,30 @@
+library(rplexos)
+context("Process files")
+
+loc <- location_input_rplexos()
+locXML <- file.path(loc, "three_nodes.xml")
+locDB  <- file.path(loc, "three_nodes-input.db")
+
+loc2 <- location_solution_rplexos()
+loc2ZIP <- file.path(loc2, "Model_Base_Solution.zip")
+loc2DB  <- file.path(loc2, "Model_Base_Solution-rplexos.db")
+
+locERR <- system.file("extdata", package = "rplexos")
+
+test_that("Process database", {
+  expect_true(process_folder(loc))
+  expect_equal(process_input(locXML), locDB)
+})
+
+test_that("Process solution", {
+  expect_true(process_folder(loc2))
+  expect_equal(process_solution(loc2ZIP), loc2DB)
+})
+
+test_that("Process vector of folders", {
+  expect_true(process_folder(c(loc, loc2)))
+})
+
+test_that("Expected errors", {
+  expect_error(process_folder(locERR))
+})
